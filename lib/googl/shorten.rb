88fd0a3711cec29b44052cba864eefb86504a355
Jesse Herrick <jessegrantherrick@gmail.com>
2013-11-23 12:39:29 -0500

    It actually works as an instance method now.

--- lib/googl/shorten.rb
+++ lib/googl/shorten.rb
@@ -1,7 +1,15 @@
-require 'net/http'
+require 'uri'
+require 'json'
+require 'rest-client'
 
 class Googl
 	def shorten
+		url = @@url
+		request = {:longUrl => url}
+
+		response = RestClient.post("https://www.googleapis.com/urlshortener/v1/url", request.to_json, :content_type => :json, :accept => :json)
 		
+		json_response = JSON.parse(response.body)
+		json_response["id"]
 	end
 end
