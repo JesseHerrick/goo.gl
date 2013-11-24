e0af433dbbf4d990f56b8ca5d5eb4ffed8f688a0
Jesse Herrick <jessegrantherrick@gmail.com>
2013-11-23 23:03:46 -0500

    Create test command.

---
+++ test/test_expand.rb
@@ -0,0 +1,11 @@
+require 'test/unit'
+require 'lib/googl'
+
+class TestExpand < Test::Unit::TestCase
+	def test_expand
+		link = Googl.new
+		link.url = "http://goo.gl/1CtCJa"
+		expected = link.expand
+		assert_equal(expected, "http://www.jessegrant.net")
+	end
+end
