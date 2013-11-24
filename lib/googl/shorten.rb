a7a87848cdc41e42cef6efeefee5ad1c2258da43
Jesse Herrick <jessegrantherrick@gmail.com>
2013-11-23 22:58:06 -0500

    Shorten actually works now.

--- lib/googl/shorten.rb
+++ lib/googl/shorten.rb
@@ -3,16 +3,16 @@ require 'json'
 require 'rest-client'
 
 class Googl
+	attr_accessor :url
 	def shorten
-		url = @@url
 		request = {:longUrl => url}
 
-		response = RestClient.post("https://www.googleapis.com/urlshortener/v1/url", request.to_json, :content_type => :json, :accept => :json)
-		if response.code == 200
-			json_response = JSON.parse(response.body)
-			json_response["id"]
+		@response = RestClient.post("https://www.googleapis.com/urlshortener/v1/url", request.to_json, :content_type => :json, :accept => :json)
+		if @response.code == 200
+			@json_response = JSON.parse(@response.body)
+			return @json_response["id"]
 		else
-			abort "ERROR: #{response.code}"
+			abort "ERROR: #{@response.code}"
 		end
 	end
 end
